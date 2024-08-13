68fdc7e939a91e608b2070d4c78e7640587e1158
IgnacioNavarro <inavarroblazquez@gmail.com>
2024-08-13 21:03:32 +0200

    added a function to write directly to db from json

---
+++ zest-app/components/utils/data.tsx
@@ -0,0 +1,434 @@
+export const data = {
+  "2": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Master your mind or it will master you.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Domina tu mente o te dominará a ti.",
+      "imageUrl": ""
+    }
+  },
+  "3": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Self-love is discipline.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "El amor propio es disciplina.",
+      "imageUrl": ""
+    }
+  },
+  "4": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Don’t yield. Don’t dilute yourself. Don’t try to make it logical.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "No cedas. No te diluyas. No trates de hacerlo lógico.",
+      "imageUrl": ""
+    }
+  },
+  "5": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Pursue your passions ruthlessly.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Persigue tus pasiones de forma despiadada.",
+      "imageUrl": ""
+    }
+  },
+  "6": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Be the person who starts first.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Sé la persona que empieza primero.",
+      "imageUrl": ""
+    }
+  },
+  "7": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Only what you do is all that matters.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Solo lo que haces es todo lo que importa.",
+      "imageUrl": ""
+    }
+  },
+  "8": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "I'm going to win because I'm crazy enough to think I can.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Voy a ganar porque estoy lo suficientemente loco para pensar que puedo.",
+      "imageUrl": ""
+    }
+  },
+  "9": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Do less, but do it better.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Haz menos, mejor.",
+      "imageUrl": ""
+    }
+  },
+  "10": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Damn, you've worked hard.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Joder, has trabajado duro.",
+      "imageUrl": ""
+    }
+  },
+  "11": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Don’t let success go to your head.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "No permitas que los éxitos te lleguen a la cabeza.",
+      "imageUrl": ""
+    }
+  },
+  "12": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Don’t let failures go to your heart.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "No permitas que los fracasos te lleguen al corazón.",
+      "imageUrl": ""
+    }
+  },
+  "13": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Destroy what destroys you.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Destruye lo que te destruye.",
+      "imageUrl": ""
+    }
+  },
+  "14": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "If you come from below, you're going to have to push harder than the rest.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Si vienes desde más abajo vas a tener que empujar más que el resto.",
+      "imageUrl": ""
+    }
+  },
+  "15": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Appreciate the bad cards, they make you a better player.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Agradece las malas cartas, te hacen mejor jugador.",
+      "imageUrl": ""
+    }
+  },
+  "16": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Train yourself to feel comfortable in the uncomfortable.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Entrénate para sentirte cómodo en lo incómodo.",
+      "imageUrl": ""
+    }
+  },
+  "17": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Everything is mental.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Todo es mental.",
+      "imageUrl": ""
+    }
+  },
+  "18": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Make it happen.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Haz que suceda.",
+      "imageUrl": ""
+    }
+  },
+  "19": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Stop focusing on things you can't control.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Deja de centrarte en las cosas que no puedes controlar.",
+      "imageUrl": ""
+    }
+  },
+  "20": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Wake up.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Despierta.",
+      "imageUrl": ""
+    }
+  },
+  "21": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Your life is your message.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Tu vida es tu mensaje.",
+      "imageUrl": ""
+    }
+  },
+  "22": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Mistakes happen. Don’t get paralyzed by them.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Los errores ocurren. No te paralices por ellos.",
+      "imageUrl": ""
+    }
+  },
+  "23": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Discipline is about giving up what you want now for what you want forever.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "La disciplina consiste en renunciar a lo que quieres ahora por lo que quieres por siempre.",
+      "imageUrl": ""
+    }
+  },
+  "24": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "The bold win.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "El atrevido, gana.",
+      "imageUrl": ""
+    }
+  },
+  "25": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Turn tragedies into triumphs.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Convierte las tragedias en triunfos.",
+      "imageUrl": ""
+    }
+  },
+  "26": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "People who avoid failure miss out on success.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Las personas que evitan el fracaso, se pierden el éxito.",
+      "imageUrl": ""
+    }
+  },
+  "27": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Learn. Create. Obsess with no finish line.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Aprender. Crear. Obsesionarse sin línea de meta.",
+      "imageUrl": ""
+    }
+  },
+  "28": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Two options. Give up or keep fighting. Choose.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Dos opciones. Darte por vencido o seguir luchando. Elige.",
+      "imageUrl": ""
+    }
+  },
+  "29": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Erasing history. Building records.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Borrando historia. Construyendo récords.",
+      "imageUrl": ""
+    }
+  },
+  "30": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Never forget how badly you wanted the things you have today.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Nunca olvides lo fuerte que deseabas las cosas que tienes hoy.",
+      "imageUrl": ""
+    }
+  },
+  "31": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Add so much value that they can't ignore you.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Aporta tanto valor que no puedan ignorarte.",
+      "imageUrl": ""
+    }
+  },
+  "32": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Never forget the why.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Jamás olvides el por qué.",
+      "imageUrl": ""
+    }
+  },
+  "33": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Trust the what.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Confía en el qué.",
+      "imageUrl": ""
+    }
+  },
+  "34": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "To hell with the when.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Que le jodan al cuándo.",
+      "imageUrl": ""
+    }
+  },
+  "35": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "Just when you are most afraid to jump is when you jump.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Justo cuando más miedo sientes por saltar, es cuando saltas.",
+      "imageUrl": ""
+    }
+  },
+  "36": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "People never forget how you make them feel.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "La gente nunca olvida cómo les haces sentir.",
+      "imageUrl": ""
+    }
+  },
+  "37": {
+    "en": {
+      "title": "⚡Zest⚡",
+      "text": "It's easy to be patient when you know what you are destined for.",
+      "imageUrl": ""
+    },
+    "es": {
+      "title": "⚡Zest⚡",
+      "text": "Es fácil ser paciente cuando sabes a qué estás destinado.",
+      "imageUrl": ""
+    }
+  }
+}
--- zest-app/components/utils/fetchFromDb.tsx
+++ zest-app/components/utils/fetchFromDb.tsx
@@ -1,5 +1,6 @@
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 import { db } from "../firebase/firebase";
+import { data } from './data';
 
 
 export type ZestDbNotification = {
@@ -21,6 +22,17 @@ export async function fetchNotification(document:string) {
     const docSnap = await getDoc(docRef);
     
     const res: ZestDbNotification = docSnap.data() as ZestDbNotification;
-    
+    readJsonFile();
     return res;
+}
+
+//use it to write data. Update the rules first and the numbers for the ids
+export async function readJsonFile() {
+    const sample = data;
+    for (let i = 2; i < 38; i++) {
+        const docRef = doc(db, 'Notifications', i.toString());
+        await setDoc(docRef, sample[i]);
+        console.log('Document written with ID: ', i);
+    }
+    return sample;
 }
